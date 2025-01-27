2d98ff28c794fb9bed33e7468a87953f4b58a7c4
JohaoG10 <johao.gavilanes@udla.edu.ec>
2025-01-26 21:52:44 -0500

    CAMBIO CARRITO VIEW MODEL

--- ProyectoP2/CarritoPage.xaml.cs
+++ ProyectoP2/CarritoPage.xaml.cs
@@ -1,34 +1,13 @@
-using ProyectoP2.Models;
-using ProyectoP2.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.Maui.Controls;
 
 namespace ProyectoP2.Paginas
 {
     public partial class CarritoPage : ContentPage
     {
-        public List<Producto> ProductosEnCarrito { get; set; }
-        public decimal Total { get; set; }
-
         public CarritoPage()
         {
             InitializeComponent();
-            ProductosEnCarrito = CarritoService.GetProductosEnCarrito().ToList();
-            Total = CarritoService.CalcularTotal();
-            BindingContext = this;
-        }
-
-        private async void OnFinalizarCompraClicked(object sender, EventArgs e)
-        {
-            // Limpiar el carrito
-            CarritoService.LimpiarCarrito();
-
-            // Mostrar alerta de éxito
-            await DisplayAlert("Compra Realizada", "Su compra ha sido realizada con éxito.", "OK");
-
-            // Regresar a la página principal o de inicio
-            await Navigation.PopToRootAsync();
+            BindingContext = new CarritoPageViewModel(); // Vincula el ViewModel con la vista
         }
     }
 }
---
+++ ProyectoP2/ViewModels/CarritoPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using ProyectoP2.Models;
+using ProyectoP2.Services;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+
+namespace ProyectoP2.Paginas
+{
+    public class CarritoPageViewModel : BindableObject
+    {
+       
+        public ObservableCollection<Producto> ProductosEnCarrito { get; set; } = CarritoService.GetProductosEnCarrito();
+
+       
+        public double Total
+        {
+            get
+            {
+                return ProductosEnCarrito.Sum(p => (double)p.Precio); 
+            }
+        }
+
+        
+        public ICommand OnFinalizarCompraClicked { get; }
+
+       
+        public CarritoPageViewModel()
+        {
+            
+            OnFinalizarCompraClicked = new Command(FinalizarCompra);
+        }
+
+       
+        private void FinalizarCompra()
+        {
+            
+            CarritoService.LimpiarCarrito();
+
+            
+            Application.Current.MainPage.DisplayAlert("Compra Realizada", "Su compra se ha realizado con éxito", "OK");
+        }
+    }
+}
